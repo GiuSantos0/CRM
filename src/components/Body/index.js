c865fb0d1259417a07d00ddde94c37c5c54c7ee7
Giulia <giulia.santos@gfai.com.br>
2019-10-25 18:53:46 -0300

    correcoes login

--- src/components/Body/index.js
+++ src/components/Body/index.js
@@ -3,9 +3,41 @@ import { createGlobalStyle } from 'styled-components'
 const Body = createGlobalStyle`
 body {
     background: #e6e6e6 url('${props => props.background}') no-repeat !important;
-    background-size: cover;
     -webkit-font-smoothing: antialiased !important;
     background-position: 81% 1%;
 }
+
+@media screen and (max-width:1200px){
+    body{
+       background: #000 url('../../assets/img/wallpaper.png') no-repeat;
+       background-position: 81% 22%;
+       background-size: 410% ;
+       position: relative;
+    }
+ }
+
+@media screen and (max-width:480px){
+    body{
+       background: #000 url('../../assets/img/wallpaper.png') no-repeat;
+       background-position: 81% 22%;
+       background-size: 410% ;
+       position: relative;
+    }
+ }
+ 
+ @media screen and (min-width:1300px){
+    body{
+        background: #000 url('../../assets/img/wallpaper.png') no-repeat;
+        background-position: 81% 22%;
+        background-size: 100% !important;
+     }
+  }
+ 
+ @media screen and (min-height:720px){
+   body{
+     background-position: 81% 11%;
+     background-size: 122%;
+   }
+ }
 `
 export default Body;
