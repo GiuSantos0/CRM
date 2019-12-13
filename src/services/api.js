c1d64b38455b16d6daf8587ca46c4b2fc7f06c1e
Giulia <giulia.santos@gfai.com.br>
2019-12-13 16:43:59 -0300

    tags html e reparo no css

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://34.68.36.127"
+  baseURL: "https://34.68.36.127"
 });
 
 api.interceptors.request.use(async config => {
